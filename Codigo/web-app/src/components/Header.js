9522a3b94fff01e3f51e6ebc98b6c3607f73bbab
rodrigo-prado1705 <rodrigo.prado1705@gmail.com>
2021-04-16 16:07:08 -0300

    Atualização header.css

--- Codigo/web-app/src/components/Header.js
+++ Codigo/web-app/src/components/Header.js
@@ -1,15 +1,13 @@
+import "./Header.css";
 import React from "react";
 import { Link, BrowserRouter as Router } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="ui secondary pointing menu">
+    <div className="Header ui secondary pointing menu">
       <Router>
         <Link to="/documento" className="item">
           Manuais
-        </Link>
-        <Link to="/bloco" className="item">
-          Blocos
         </Link>
         <Link to="/" className="right item">
           Logout
