3a043503884f37466dcf054a03df320d79d09fab
rodrigo-prado1705 <rodrigo.prado1705@gmail.com>
2021-04-16 09:03:09 -0300

    Correção nome classe DocumentoRepository API

--- Codigo/rest-api/src/main/java/com/grupo2/API_TraceFinder/repository/documentoRepository.java
+++ Codigo/rest-api/src/main/java/com/grupo2/API_TraceFinder/repository/documentoRepository.java
@@ -11,12 +11,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DocumentoRepository extends JpaRepository<Documento, Long> {
-	
-	// Contains traz tudo que contem substituindo o LIKE %% do mysql
-	List<Documento> findBydocumentonomeContains(String documentonome);
-	
-	@Query(value = "SELECT documento_id, documento_nome, documento_pn, documento_caminho FROM documento"
-					+ " WHERE documento_nome LIKE %?1% AND documento_pn LIKE %?2% ", nativeQuery = true)
-	List<Documento> SelectDocumentoLikeNomePn(String docnome, String docpn);
+
+  // Contains traz tudo que contem substituindo o LIKE %% do mysql
+  List<Documento> findBydocumentonomeContains(String documentonome);
+
+  @Query(value = "SELECT documento_id, documento_nome, documento_pn, documento_caminho FROM documento"
+      + " WHERE documento_nome LIKE %?1% AND documento_pn LIKE %?2% ", nativeQuery = true)
+  List<Documento> SelectDocumentoLikeNomePn(String docnome, String docpn);
 
 }
