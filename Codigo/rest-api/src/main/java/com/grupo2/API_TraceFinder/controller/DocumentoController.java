c78167927c0b8c9bc43ca9a6fcf9619edc671aa0
rodrigo-prado1705 <rodrigo.prado1705@gmail.com>
2021-04-16 16:07:56 -0300

    Atualização header.css

--- Codigo/rest-api/src/main/java/com/grupo2/API_TraceFinder/controller/DocumentoController.java
+++ Codigo/rest-api/src/main/java/com/grupo2/API_TraceFinder/controller/DocumentoController.java
@@ -24,7 +24,6 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/documentos")
-<<<<<<< HEAD
 public class DocumentoController {
 
   private DocumentoRepository documentoRepository = null;
@@ -76,11 +75,24 @@ public class DocumentoController {
     return doc.stream().map((BlList) -> DocumentoRs.converter(BlList)).collect(Collectors.toList());
   }
 
+  // SELECT com Query//
+  @GetMapping("/joinbloco")
+  public List<DocumentoRs> selectJoin(@RequestParam(value = "documentoid", required = false) Long docid) {
+    var documento = documentoRepository.SelectJoinBloco(docid);
+    return documento.stream().map((BlList) -> DocumentoRs.converter(BlList)).collect(Collectors.toList());
+  }
+
+  // SELECT Documentos que contem aquele bloco//
+  @GetMapping("/documentobloco")
+  public List<DocumentoRs> selectDocBloco(@RequestParam(value = "blocoid", required = false) Long blocoid) {
+    var documento = documentoRepository.SelectDocBloco(blocoid);
+    return documento.stream().map((BlList) -> DocumentoRs.converter(BlList)).collect(Collectors.toList());
+  }
+
   // INSERT //
   @PostMapping("/")
   public void insertDocumento(@RequestBody DocumentoRq documento) {
     var doc = new Documento();
-
     doc.setDocumentonome(documento.getDocumentonome());
     doc.setDocumentopn(documento.getDocumentopn());
     doc.setDocumentocaminho(documento.getDocumentocaminho());
@@ -109,216 +121,4 @@ public class DocumentoController {
     documentoRepository.deleteById(id);
   }
 
-=======
-public class DocumentoController 
-{
-	
-	private DocumentoRepository documentoRepository = null;
-	private DocumentoCustomRepository documentoCustomRepository = null;
-		
-	public DocumentoController(DocumentoRepository documentoRepository, DocumentoCustomRepository documentoCustomRepository) 
-	{
-		this.documentoRepository = documentoRepository;
-		this.documentoCustomRepository = documentoCustomRepository;
-	}
-		
-	
-	// SELECT de todos//
-	@GetMapping("/")
-	public List<DocumentoRs> selectAll()
-	{
-		var documentos = documentoRepository.findAll();
-		return documentos.stream().map((doc) -> DocumentoRs.converter(doc)).collect(Collectors.toList());	
-	}
-	
-	// SELECT por ID //
-	@GetMapping("/{id}")
-	public DocumentoRs selectID(@PathVariable("id") Long id)
-	{
-		var doc = documentoRepository.getOne(id);
-		return DocumentoRs.converter(doc);
-	}
-	
-	// SELECT por Nome
-	@GetMapping("/filtronome")
-	public List<DocumentoRs> findDocumentoBydocumentonome(@RequestParam("documentonome") String documentonome)
-	{
-		return this.documentoRepository.findBydocumentonomeContains(documentonome)
-				.stream()
-				.map(DocumentoRs::converter)
-				.collect(Collectors.toList());
-	}
-	
-		
-	// SELECT CUSTOM por Nome e PN
-	@GetMapping("/filtronomepn")
-	public List<DocumentoRs> findDocumentoBydocumentonomepn(
-			@RequestParam(value = "documentoid", required = false) Long documentoid, 
-			@RequestParam(value = "documentonome", required = false) String documentonome,
-			@RequestParam(value = "documentopn", required = false) String documentopn,
-			@RequestParam(value = "documentocaminho", required = false) String documentocaminho
-	)
-	{
-		return this.documentoCustomRepository.find(documentoid, documentonome, documentopn, documentocaminho)
-				.stream()
-				.map(DocumentoRs::converter)
-				.collect(Collectors.toList());
-	}
-	
-	
-	// SELECT com Query//
-	@GetMapping("/likenomepn")
-	public List<DocumentoRs> SelectLike(@RequestParam(value = "docnome", required = false) String docnome,
-			@RequestParam(value = "docpn", required = false) String docpn)
-	{
-		var doc = documentoRepository.SelectDocumentoLikeNomePn(docnome, docpn);
-		return doc.stream().map((BlList) -> DocumentoRs.converter(BlList)).collect(Collectors.toList());	
-	}
-	
-	
-	// SELECT com Query//
-	@GetMapping("/joinbloco")
-	public List<DocumentoRs> selectJoin(@RequestParam(value = "documentoid", required = false) Long docid)
-	{
-		var documento = documentoRepository.SelectJoinBloco(docid);
-		return documento.stream().map((BlList) -> DocumentoRs.converter(BlList)).collect(Collectors.toList());	
-	}
-	
-	
-	// SELECT Documentos que contem aquele bloco//
-	@GetMapping("/documentobloco")
-	public List<DocumentoRs> selectDocBloco(@RequestParam(value = "blocoid", required = false) Long blocoid)
-	{
-		var documento = documentoRepository.SelectDocBloco(blocoid);
-		return documento.stream().map((BlList) -> DocumentoRs.converter(BlList)).collect(Collectors.toList());	
-	}
-	
-	
-	
-	// INSERT //
-	@PostMapping("/")
-	public void insertDocumento(@RequestBody DocumentoRq documento)
-	{
-		var doc = new Documento();
-		doc.setDocumentonome(documento.getDocumentonome());
-		doc.setDocumentopn(documento.getDocumentopn());
-		doc.setDocumentocaminho(documento.getDocumentocaminho());
-		documentoRepository.save(doc);
-	}
-	
-	// UPDATE
-	@PutMapping("/{id}")
-	public void updateDocumento(@PathVariable Long id, @RequestBody DocumentoRq documento) throws Exception
-	{
-		var doc = documentoRepository.findById(id);
-		
-		if(doc.isPresent())
-		{
-			var doc2 = doc.get();
-			doc2.setDocumentonome(documento.getDocumentonome());
-			doc2.setDocumentopn(documento.getDocumentopn());
-			doc2.setDocumentocaminho(documento.getDocumentocaminho());
-			documentoRepository.save(doc2);
-		}
-		else { throw new Exception("Documento não encontrado"); }
-	}
-	
-	// DELETE
-	@DeleteMapping("/{id}")
-	public void deleteDocumento(@PathVariable Long id)
-	{	
-		documentoRepository.deleteById(id);
-	}
-
-
-	
-	
->>>>>>> main
 }
-
-
-  
-
-
- 
-
-  
-  
-
-  
-       
-    
-    
-  
-
-  
-  
-   
-    
-    
-  
-
-  
-  
-   
-    
-    
-  
-
-  
-  
-   
-    
-        
-  
-
-  
-  
-  
-      
-      
-      
-       
-    
-        
-  
-
-  
-  
-  
-       
-    
-    
-  
-
-  
-  
-   
-    
-
-    
-    
-    
-    
-  
-
-  
-  
-   
-    
-
-      
-      
-      
-      
-      
-      
-     
-      
-    
-  
-
-  
-  
-
-  
