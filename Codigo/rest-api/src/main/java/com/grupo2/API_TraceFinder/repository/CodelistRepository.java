483a694c67691fa14a593ddded01913b87f3cb79
rodrigo-prado1705 <rodrigo.prado1705@gmail.com>
2021-05-26 11:59:36 -0300

    Correção pesquisa codelist

--- Codigo/rest-api/src/main/java/com/grupo2/API_TraceFinder/repository/CodelistRepository.java
+++ Codigo/rest-api/src/main/java/com/grupo2/API_TraceFinder/repository/CodelistRepository.java
@@ -11,41 +11,44 @@ import com.grupo2.API_TraceFinder.classes.Codelist;
 
 @Repository
 public interface CodelistRepository extends JpaRepository<Codelist, Long> {
-	
-	@Query(value = "SELECT codelist.codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
-			+ "	FROM codelist WHERE documento_id = ?1  ", nativeQuery = true)
-	List<Codelist> SelectCodelistDoc(Long id);
-	
-	@Query(value = "SELECT codelist.codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
-			+ "	FROM codelist "
-			+ " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id"
-			+ " WHERE traco_id = ?1  ", nativeQuery = true)
-	List<Codelist> SelectTracoCodelist(Long id); 
-	
-	@Query(value = "SELECT codelist.codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
-			+ "	FROM codelist WHERE codelist_id = ?1  ", nativeQuery = true)
-	List<Codelist> SelectCodelist(Long id);
-		
-	/*@Query(value = "SELECT codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
-			+ "	FROM codelist "
-			+ " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id "
-			+ " WHERE documento_id = ?1" + " AND traco_id = ?2 ;", nativeQuery = true)
-	List<Codelist> SelectBlocosTraco(Long docId, Long tracoId);*/
-	
-	//Pesquisa de blocos com o traço
-	@Query(value = "SELECT traco_doc_nome, traco_doc_codigo, codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
-			+ "	FROM codelist "
-			+ " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id "
-			+ " INNER JOIN traco_doc ON traco_doc.traco_doc_id = relacao_bloco_traco.bloco_id "
-			+ " WHERE documento_id = ?1" + " AND traco_id = ?2 ;", nativeQuery = true)
-	List<Codelist> SelectBlocosTraco(Long docId, Long tracoId);
-	
-	/*
-	@Query(value = "SELECT traco_doc_nome, traco_doc_codigo, codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
-			+ "	FROM codelist "
-			+ " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id "
-			+ " INNER JOIN traco_doc ON traco_doc.traco_doc_id = relacao_bloco_traco.bloco_id "
-			+ " WHERE documento_id = ?1" + " AND traco_id = ?2 ;", nativeQuery = true)
-	ModelAndView SelectBlocosTraco2(Long docId, Long tracoId);
-	*/
+
+  @Query(value = "SELECT codelist.codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
+      + "	FROM codelist WHERE documento_id = ?1  ", nativeQuery = true)
+  List<Codelist> SelectCodelistDoc(Long id);
+
+  @Query(value = "SELECT codelist.codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
+      + "	FROM codelist " + " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id"
+      + " WHERE traco_id = ?1  ", nativeQuery = true)
+  List<Codelist> SelectTracoCodelist(Long id);
+
+  @Query(value = "SELECT codelist.codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
+      + "	FROM codelist WHERE codelist_id = ?1  ", nativeQuery = true)
+  List<Codelist> SelectCodelist(Long id);
+
+  /*
+   * @Query(value =
+   * "SELECT codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
+   * + "	FROM codelist " +
+   * " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id "
+   * + " WHERE documento_id = ?1" + " AND traco_id = ?2 ;", nativeQuery = true)
+   * List<Codelist> SelectBlocosTraco(Long docId, Long tracoId);
+   */
+
+  // Pesquisa de blocos com o traço
+  @Query(value = "SELECT traco_doc_nome, traco_doc_codigo, codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
+      + "	FROM codelist " + " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id "
+      + " INNER JOIN traco_doc ON traco_doc.traco_doc_id = relacao_bloco_traco.bloco_id " + " WHERE documento_id = ?1"
+      + " AND traco_id = ?2 ;", nativeQuery = true)
+  List<Codelist> SelectBlocosTraco(Long docId, Long tracoId);
+
+  /*
+   * @Query(value =
+   * "SELECT traco_doc_nome, traco_doc_codigo, codelist_id, codelist_secao, codelist_subsecao, codelist_nomebloco, codelist_codebloco, codelist_caminho, documento_id "
+   * + "	FROM codelist " +
+   * " INNER JOIN relacao_bloco_traco ON relacao_bloco_traco.bloco_id = codelist.codelist_id "
+   * +
+   * " INNER JOIN traco_doc ON traco_doc.traco_doc_id = relacao_bloco_traco.bloco_id "
+   * + " WHERE documento_id = ?1" + " AND traco_id = ?2 ;", nativeQuery = true)
+   * ModelAndView SelectBlocosTraco2(Long docId, Long tracoId);
+   */
 }
